6917623eff47cc9228035f3d364cc19eda6d470c
Andrew <andrewgardner@live.com>
2018-12-04 13:21:00 -0700

    pre break upload

---
+++ src/model/Chat.java
@@ -0,0 +1,36 @@
+package model;
+import java.util.ArrayList;
+public class Chat {
+	
+	//lists-----------------------------------------
+	private void responseList(){
+		ArrayList<String> response = new ArrayList<String>();
+		// add responses here
+	}
+	
+	private void spookyList() {
+		ArrayList<String> spooky = new ArrayList<String>();
+		// add spooky things here
+	}
+//strings-----------------------------------	
+	private String content(){
+		return "hi";
+	}
+	
+	private String currentUser() {
+		return "elmo";		
+	}
+	
+	private String joke() {
+		return "you";
+	}
+	//---------------------------------
+	//constructors
+	public void Chatbot() {
+		
+	}
+	
+	
+	
+
+}
